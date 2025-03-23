6ca406bc9f0e5c64f52aae8ad19b28179c3b6832
il4nn <ilan.r.nissim@gmail.com>
2025-03-23 23:22:30 +0100

    made progress on analytics side

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+from analytics import *
 import streamlit as st
+import pandas as pd  
 
 st.title('Hello World!')
+
+models = st.selectbox(
+    'Choose a model?',
+    ('llama-3.3-70b-specdec', 'llama-3.3-70b', 'llama-3.3-70b-ftb')
+)
+
+st.write(f'You selected: {models}')
+
+match models:
+    case 'llama-3.3-70b-specdec': 
+        df = parse_results(models)
+        st.dataframe(df)
+    case _:
+        pass
+
+
