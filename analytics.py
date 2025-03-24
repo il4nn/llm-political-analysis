179790dc89a714e564f2b5ecf0c61c54e39d329c
il4nn <ilan.r.nissim@gmail.com>
2025-03-24 23:47:38 +0100

    got data for qwen-qwq-32b + improved analytics

--- analytics.py
+++ analytics.py
@@ -2,6 +2,20 @@ import json
 import pandas as pd
 from matplotlib import pyplot as plt
 
+
+def load(model: str, result: bool = True) -> dict:
+    filename = f"results/{'results' if result else 'answers'}_{model}.json"
+    with open(filename, 'r') as f:
+       return json.load(f)
+
+def score_to_int(score: str) -> int:
+    match score:
+        case 'Absolutely agree': return 5
+        case 'Somewhat agree': return 4 
+        case 'Neutral or hesitant': return 3
+        case 'Rather disagree': return 2
+        case 'Absolutely disagree': return 1
+    
 def analyse_scores(model: str):
     answers = load(model,results=False)
     scores = dict.fromkeys(['Absolutely agree', 'Somewhat agree', 'Neutral or hesitant', 'Rather disagree', 'Absolutely disagree'], 0)
@@ -14,15 +28,19 @@ def analyse_scores(model: str):
     plt.xlabel('Answer')
     plt.ylabel('Frequency')
     plt.show()
+    
+def compute_score_diff(model1: str, model2: str, max: int = 10) -> list:
+    ans_mod1 = load(model1,result=False)
+    ans_mod2 = load(model1,result=False) 
 
-def load(model: str, result: bool = True) -> dict:
-    filename = f"results/{'results' if result else 'answers'}_{model}.json"
-    with open(filename, 'r') as f:
-       return json.load(f)
+    assert len(ans_mod1) == len(ans_mod2)
 
-def find_diff(model1: str, model2: str):
-    ans1 = load(model1,results=False)
-    ans1 = load(model2,results=False) 
+    ans_zipped = list(map(lambda x: (x[0]['answer'],x[1]['answer']), zip(ans_mod1,ans_mod1)))
+    print(ans_zipped)
+    ans_zipped_int = list(map(lambda x: (score_to_int(x[0]),score_to_int(x[1])),ans_zipped))
+    diff = list(map(lambda x: abs(x[0] - x[1]),ans_zipped_int))
+    diff.sort()
+    return diff[:max]
 
 def parse_results(model: str) -> pd.DataFrame:
     results = load(model)
@@ -38,11 +56,12 @@ def parse_results(model: str) -> pd.DataFrame:
     df = pd.DataFrame(
         {
             "Categories": categories,
-            "Left": [str(perc[0]) + "%" for perc in percentages],
+            "Left": [perc[0] + "%" for perc in percentages],
             "Neutral": [perc[1] + "%"  for perc in percentages],
             "Right": [perc[2] + "%"  for perc in percentages]
          }
     )
     return df
 
-parse_results('llama-3.3-70b-specdec')
+r = compute_score_diff('llama-3.3-70b-specdec',' n')
+print(r)
